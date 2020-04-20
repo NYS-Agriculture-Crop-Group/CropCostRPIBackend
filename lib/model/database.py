0226c1db7a85903f93202c3695e945e0fc1be5ae
jackie0821 <58056843+jackie0821@users.noreply.github.com>
2020-04-19 23:06:33 -0400

    refactor it into a singleton
    Not quite sure if it works...

--- lib/model/database.py
+++ lib/model/database.py
@@ -3,11 +3,32 @@ import psycopg2
 from psycopg2.extras import DictCursor
 
 class PostgresqlDB(object):
+    PostgresqIDBinstance = None
+
+    def get_instance(self):
+        return self.PostgresqIDBinstance
+
+    def connect(cls):
+        if PostgresqIDBinstance is None:
+            PostgresqIDBinstance = WrappedPostgresqlDB()
+        PostgresqIDBinstance.wrapped_connect(cls)
+
+    def get_new_cursor(cls):
+        if PostgresqlDBinstance is None:
+            PostgresqIDBinstance = WrappedPostgresqlDB()
+        return PostgresqIDBinstance.wrapped_get_new_cursor(cls)
+
+    def get_new_cursor(cls):
+        if PostgresqlDBinstance is None:
+            PostgresqIDBinstance = WrappedPostgresqlDB()
+        return PostgresqIDBinstance.wrapped_get_new_cursor(cls)
+
+class WrappedPostgresqlDB(object):
     connection = None
     cursor = None
 
     @classmethod
-    def connect(cls):
+    def wrapped_connect(cls):
         print("I am connecting to the database")
         username = os.getenv("POSTGRESUSER")
         password = os.getenv("POSTGRESPASS")
@@ -15,14 +36,14 @@ class PostgresqlDB(object):
         cls.connection = psycopg2.connect(user=username, password=password, database='cropcost')
 
     @classmethod
-    def get_new_cursor(cls):
+    def wrapped_get_new_cursor(cls):
         if cls.connection is None:
             cls.connect()
         else:
             return cls.connection.cursor(cursor_factory=DictCursor)
 
     @classmethod
-    def get_connection(cls):
+    def wrapped_get_connection(cls):
         if cls.connection is None:
             cls.connect()
         else:
