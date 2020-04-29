3fa7c27ec1781cb024e9686ec249c8d793d0e0c8
Tonychen050400 <61198124+Tonychen050400@users.noreply.github.com>
2020-04-28 22:07:16 -0400

    Update database.py

--- lib/model/database.py
+++ lib/model/database.py
@@ -5,23 +5,21 @@ from psycopg2.extras import DictCursor
 class PostgresqlDB(object):
     PostgresqIDBinstance = None
 
-    def get_instance(self):
-        return self.PostgresqIDBinstance
+    @classmethod
+    def get_instance(cls):
+        return cls.PostgresqIDBinstance
 
+    @classmethod
     def connect(cls):
-        if PostgresqIDBinstance is None:
-            PostgresqIDBinstance = WrappedPostgresqlDB()
-        PostgresqIDBinstance.wrapped_connect(cls)
+        if cls.PostgresqIDBinstance is None:
+            cls.PostgresqIDBinstance = WrappedPostgresqlDB()
+        return cls.PostgresqIDBinstance.wrapped_connect()
 
+    @classmethod
     def get_new_cursor(cls):
-        if PostgresqlDBinstance is None:
-            PostgresqIDBinstance = WrappedPostgresqlDB()
-        return PostgresqIDBinstance.wrapped_get_new_cursor(cls)
-
-    def get_new_cursor(cls):
-        if PostgresqlDBinstance is None:
-            PostgresqIDBinstance = WrappedPostgresqlDB()
-        return PostgresqIDBinstance.wrapped_get_new_cursor(cls)
+        if cls.PostgresqIDBinstance is None:
+            cls.PostgresqIDBinstance = WrappedPostgresqlDB()
+        return cls.PostgresqIDBinstance.wrapped_get_new_cursor()
 
 class WrappedPostgresqlDB(object):
     connection = None
