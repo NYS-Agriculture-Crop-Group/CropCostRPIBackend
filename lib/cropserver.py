e51e352201f3284df2a653a57f020792f0dd2860
Tonychen050400 <61198124+Tonychen050400@users.noreply.github.com>
2020-04-28 22:20:28 -0400

    Update cropserver.py

--- lib/cropserver.py
+++ lib/cropserver.py
@@ -1,11 +1,13 @@
 from dotenv import load_dotenv
-load_dotenv()
 import os
+load_dotenv()
 from flask import Flask, Blueprint
 from flask_restful import Resource, Api
 from lib.resources.depriciatingAssets import DepriciatingAssetResource
 from lib.model.database import PostgresqlDB
 from lib.resources.authenticate import RegistationResource
+from lib.resources.users import UserResource
+from flask import request, g, Response,render_template
 
 
 app = Flask(__name__)
@@ -13,7 +15,12 @@ app.debug = True
 app.config['SECRET_KEY'] = os.getenv("SECRET")
 PostgresqlDB.connect()
 
+@app.route('/login/')
+def login(name=None):
+    return render_template('login.html', name=name)
+  
 api = Api(app)
 
 api.add_resource(DepriciatingAssetResource, '/dp')
 api.add_resource(RegistationResource, '/auth')
+api.add_resource(UserResource,'/machines')
